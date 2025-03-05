d7afd8cb98547f056e2bf56ab4220fe23c213adb
Mamadou N'Gatté <doumstyle@gmail.com>
2025-03-05 20:14:05 +0100

    form validation

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
+use App\Form\ProductType;
+
+final class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'product_index')]
+    public function index(ProductRepository $repository): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'products' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/product/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($product);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId(),
+            ]);
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+}

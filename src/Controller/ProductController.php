dbefe9b9df85aa732c6dfc8a41e8d83b2339c052
Mamadou N'Gatté <doumstyle@gmail.com>
2025-03-06 08:23:54 +0100

    add flash message when adding new product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
 use App\Form\ProductType;
 
 final class ProductController extends AbstractController
@@ -38,11 +38,14 @@ final class ProductController extends AbstractController
 
         $form->handleRequest($request);
 
+
         if ($form->isSubmitted() && $form->isValid()) {
 
             $manager->persist($product);
 
             $manager->flush();
+
+            $this->addFlash('notice', 'Product created successfully!');
 
             return $this->redirectToRoute('product_show', [
                 'id' => $product->getId(),
